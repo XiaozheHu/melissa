86e4d98491cd28ae4d689ed89a2b089f7649d8c8
Xiaozhe Hu <Xiaozhe.Hu@tufts.edu>
2022-01-11 06:48:22 -0500

    Xiaozhe

---
+++ code/show_results.m
@@ -0,0 +1,17 @@
+[val,idx] = max(mean(acc_mu));
+fprintf('Mashup:  acc = %f, dim = %d\n', val, dim(idx));
+
+[val,idx] = max(mean(acc_melissa));
+fprintf('Melissa: acc = %f, dim = %d\n', val, dim(idx));
+
+[val,idx] = max(mean(f1_mu));
+fprintf('Mashup:  f1 = %f, dim = %d\n', val, dim(idx));
+
+[val,idx] = max(mean(f1_melissa));
+fprintf('Melissa: f1 = %f, dim = %d\n', val, dim(idx));
+
+[val,idx] = max(mean(auc_mu));
+fprintf('Mashup:  auc = %f, dim = %d\n', val, dim(idx));
+
+[val,idx] = max(mean(auc_melissa));
+fprintf('Melissa: auc = %f, dim = %d\n', val, dim(idx));
