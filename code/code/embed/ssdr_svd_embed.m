df112a863a915b6a1965bc3d245fb431ea9ee36a
XiaozheHu <xiaozhehu@pop-os.localdomain>
2022-02-16 15:19:11 -0500

    Xiaozhe

--- code/code/embed/ssdr_svd_embed.m
+++ code/code/embed/ssdr_svd_embed.m
@@ -7,6 +7,7 @@ function [x, d] = ssdr_svd_embed(walks, ndim, L, flag_JL)
     epsilon = 0.5;  % accuracy 
     k = ceil((6/(epsilon^2/2 - epsilon^3/3))*log10(ngene));  % low dimension
     k = max(k, ceil(ndim/nnetworks));
+    %k = ndim;
     R_all = zeros(nnetworks*k, ngene);
   else
     R_all = zeros(nnetworks*ngene, ngene);
@@ -22,12 +23,15 @@ function [x, d] = ssdr_svd_embed(walks, ndim, L, flag_JL)
         R_all((i-1)*k+1:i*k, :) = Q*(log(W + 1/ngene));
     else
         %R_all = [R_all; (log(W + 1/ngene))];
-        R_all((i-1)*ngene+1:ngene, :) = (log(W + 1/ngene));
+        R_all((i-1)*ngene+1:i*ngene, :) = (log(W + 1/ngene));
     end
 
     clear W;
   end
   
+  %Q = generate_rand_proj( ndim, ngene*nnetworks, 'dense');
+  %R_all = Q*R_all;
+
   temp_mat = R_all*L*R_all';
   [V, d] = eigs(temp_mat, ndim);
   x = V'*R_all;
